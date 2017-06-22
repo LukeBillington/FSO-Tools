dbb8b410c3ba5b5238c97005c1073d1cab57c5d8
Luke Billington <lukecreative@me.com>
2017-06-22 17:46:05 -0400

    Updated for FSO Version 4.1
    The dates format has changed to a timestamp in FSO version 4.1, altering
    the way due dates were highlighted. In addition, spacers were added,
    disabling a selector, so this has been adjusted in this v1.1 of FSO
    Tools.

--- content.js
+++ content.js
@@ -9,20 +9,30 @@ var date = `${dMonth} ${dLong.getDate()}`;
 function findModules() {
 
   // Module Due Date Highlight
-  var dateLocation = document.querySelectorAll('.card--primary .card__column-metadata .card__meta:nth-of-type(2) .card__meta-value');
+  var dateLocation = document.querySelectorAll('.card--primary .card__column-metadata .card__meta:nth-of-type(3) .card__meta-value');
   for (var i = 0; i < dateLocation.length; i++) {
-    var pDate = dateLocation[i].parentElement.parentElement.parentElement;
-    if(dateLocation[i].innerText == date) {
-      pDate.classList.add('highlight');
+    var pCard = dateLocation[i].parentElement.parentElement.parentElement;
+    var pSrc = dateLocation[i].innerText.toUpperCase();
+    var pData = pSrc.split(" @ ");
+    console.log(pData);
+    var pDate = pData[0];
+    var pTime = pData[1];
+    if(pDate == date) {
+      pCard.classList.add('highlight');
     }
   }
 
   // Assignment Due Date Highlight
-  var dateSubLocation = document.querySelectorAll('.card--secondary .card__column-metadata .card__meta:nth-of-type(2) .card__meta-value');
+  var dateSubLocation = document.querySelectorAll('.card--secondary .card__column-metadata .card__meta:nth-of-type(3) .card__meta-value');
   for (var j = 0; j < dateSubLocation.length; j++) {
-    var vSubDate = dateSubLocation[j].parentElement.parentElement.parentElement;
-    if(dateSubLocation[j].innerText.toUpperCase() == date) {
-      vSubDate.classList.add('highlight');
+    var vSubCard = dateSubLocation[j].parentElement.parentElement.parentElement;
+    var vSubSrc = dateSubLocation[j].innerText.toUpperCase();
+    var vSubData = vSubSrc.split(" @ ");
+    console.log(vSubData);
+    var vSubDate = vSubData[0];
+    var vSubTime = vSubData[1];
+    if(vSubDate == date) {
+      vSubCard.classList.add('highlight');
     }
   }
 
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 # FSO Tools
-Recently Full Sail constructed a new LMS v4 to make keeping track of assignments easier.
+**UPDATED FOR FSO 4.1.** Recently Full Sail constructed a new LMS v4 to make keeping track of assignments easier.
 I had issues with the new interface in regards to it's usability leading me to create this Chrome extension.
 
 **Screenshots can be seen further below.**
@@ -8,6 +8,20 @@ I had issues with the new interface in regards to it's usability leading me to c
 + **Separates modules from activities.** Groups and colors assignments under modules and adds spacing between modules.
 + **Highlights today's assignments.** All assignments marked as due today will be highlighted.
 + **Adds a dark mode.** Toggle this by pressing the FSO Tools icon next to the address bar.
+
+## How to update
+If you have already installed FSO Tools for a previous version, follow these steps.
+Otherwise, if you haven't installed FSO Tools before, see the How to install section below.
+
+1. Download or clone this repository.
+2. Remove the old version of this extension off of your machine.
+3. Replace the new version in the same location as the old version was previously.
+4. In Chrome, visit [chrome://extensions](chrome://extensions).
+5. Refresh the page, make sure you see that the current version number – next to the FSO Tools title – reads 1.1 instead of 1.0.
+6. Open FSO in Chrome to see the applied changes.
+
+If you are unable to update FSO Tools with these steps, remove the extension from the page and then load it in again like steps 5 and 6 below.
+
 
 ## How to install
 1. Download or clone this repository.
